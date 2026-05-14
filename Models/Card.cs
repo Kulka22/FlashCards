5d56b63d49f5c1f2dbdae7e626bf2eabaa881d19
Vyacheslav <kuzslava2211@gmail.com>
2026-05-14 17:15:44 +0300

    added a models

---
+++ Models/Card.cs
@@ -0,0 +1,12 @@
+﻿namespace FlashCards.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public List<string> Translations { get; set; }
+        public List<string> Examples { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace FlashCards.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Card> Cards { get; set; }
+    }
+}
